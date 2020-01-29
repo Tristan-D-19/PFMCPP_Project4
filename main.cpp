7c659a4c71037e4b78713f89a9a3dd1a858e23ac
Tristan-D-19 <tristand.thomas19@gmail.com>
2020-01-29 15:32:24 +0000

    completed assignment

--- main.cpp
+++ main.cpp
@@ -32,8 +32,93 @@
  Wait for my code review.
  */
 
+
+struct FloatType{
+  float add( float lhs, float rhs ){
+   return lhs + rhs; 
+  }
+  float subtract( float lhs, float rhs ){
+    return lhs -rhs;
+  }
+  float multiply( float lhs, float rhs ){
+    return lhs * rhs;
+  }
+  float divide( float lhs, float rhs ){
+    return lhs/rhs;
+  }
+};
+
+struct DoubleType{
+ double add( double lhs, double rhs ){
+   return lhs + rhs; 
+  }
+  double subtract( double lhs, double rhs ){
+    return lhs -rhs;
+  }
+  double multiply( double lhs, double rhs ){
+    return lhs * rhs;
+  }
+  double divide( double lhs, double rhs ){
+    return lhs/rhs;
+  }
+
+};
+
+struct IntType{
+
+   int add( int lhs, int rhs ){
+   return lhs + rhs; 
+  }
+  int subtract( int lhs, int rhs ){
+    return lhs -rhs;
+  }
+  int multiply( int lhs, int rhs ){
+    return lhs * rhs;
+  }
+  int divide( int lhs, int rhs ){
+    return lhs/rhs;
+  }
+};
+
+
+
 #include <iostream>
+#include <iomanip>
 int main()
 {
+  FloatType ft;
+  DoubleType dt;
+  IntType it;
+  auto ftAddResult = ft.add(4.42f, 9.345f);
+  auto ftSubResult = ft.subtract(500.345f, 300.345f);
+  auto ftDivResult = ft.divide(23434.94f, 3.242f);
+  auto ftMulResult = ft.multiply(1.4564f, 6.35235f);
+
+  auto dtAddResult = dt.add(3.45, 5.36);
+  auto dtSubResult = dt.subtract(9.645,3.64534);
+  auto dtDivResult = dt.divide(200.35, 2.46);
+  auto dtMulResult = dt.multiply(4.452, 5.245);
+
+  auto itAddResult = it.add(35, 477);
+  auto itSubResult = it.subtract(450, 46);
+  auto itDivResult = it.divide(40,8);
+  auto itMulResult = it.multiply(5, 60);
+
+   std::cout  << "result of ft.add(): " << ftAddResult << std::endl;
+   std::cout  << "result of ft.subtract(): " << ftSubResult << std::endl;
+   std::cout  << "result of ft.divide(): " << ftDivResult << std::endl;
+   std::cout  << "result of ft.multiply(): " << ftMulResult << std::endl;
+
+   std::cout  << "result of dt.add(): " << dtAddResult << std::endl;
+   std::cout  << "result of dt.subtract(): " << dtSubResult << std::endl;
+   std::cout  << "result of dt.divide(): " << dtDivResult << std::endl;
+   std::cout  << "result of dt.multiply(): " << dtMulResult << std::endl;
+
+   std::cout  << "result of it.add(): " << itAddResult << std::endl;
+   std::cout  << "result of it.subtract(): " << itSubResult << std::endl;
+   std::cout  << "result of it.divide(): " << itDivResult << std::endl;
+   std::cout  << "result of it.multiply(): " << itMulResult << std::endl;
+
+
     std::cout << "good to go!" << std::endl;
 }
